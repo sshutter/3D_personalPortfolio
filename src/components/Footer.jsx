c650c5f5d4a653fb594013d84e17d3ed6f96a38d
shutter <6331335921@student.chula.ac.th>
2023-06-07 23:07:11 +0700

    make footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,7 @@
 import React from "react";
-import {
-  FaFacebookF,
-  FaInstagram,
-  FaEnvelope,
-  FaLinkedin,
-  FaGithub,
-} from "react-icons/fa";
+import { FaInstagram, FaEnvelope, FaLinkedin, FaGithub } from "react-icons/fa";
 import { AiOutlineMail } from "react-icons/ai";
-import { BsFillTelephoneFill } from "react-icons/bs";
+import { BsFillTelephoneFill, BsFacebook } from "react-icons/bs";
 import { styles } from "../styles";
 
 const Footer = () => {
@@ -15,66 +9,91 @@ const Footer = () => {
     window.location.href = "mailto:phakin.buddhal@gmail.com";
   };
 
-  return (
-    <footer
-      className={`${styles.paddingX} w-full items-center bg-primary`}
-      style={{ borderTop: "1px solid #9E9E9E" }}
-    >
-      <div className="container" style={styles}>
-        <div className="row py-5" style={styles}>
-          <div
-            className="col-md-6 text-secondary align-items-center justify-content-center flex-col"
-            style={(styles, { borderRight: "1px solid #9e9e9e" })}
-          >
-            <div className="d-flex align-items-center py-3">
-              <AiOutlineMail
-                color="#1A1A1A"
-                size={25}
-                className="inline pr-2"
-              />
-              phakin.buddha@gmail.com
-            </div>
+  const handleFacebookClick = () => {
+    window.open("https://www.facebook.com/phakin.buddha/", "_blank");
+  };
 
-            <div className="d-flex align-items-center py-3">
-              <BsFillTelephoneFill
-                color="#1A1A1A"
-                size={25}
-                className="inline pr-2"
-              />
-              086-398-5005
+  const handleInstagramClick = () => {
+    window.open("https://www.instagram.com/_sshutter/", "_blank");
+  };
+
+  const handleLinkedinClick = () => {
+    window.open(
+      "https://www.linkedin.com/in/phakin-buddha-a24b4823b/",
+      "_blank"
+    );
+  };
+
+  const handleGithubClick = () => {
+    window.open("https://github.com/sshutter", "_blank");
+  };
+
+  return (
+    <footer className="bg-primary" style={{ borderTop: "1px solid #9E9E9E" }}>
+      <div className="container-custom">
+        <div className="row-custom py-3">
+          <div
+            className="col-custom py-3"
+            style={{
+              borderRight: "1px solid #9E9E9E",
+              "@media (min-width: 768px)": {
+                borderRight: "0px",
+              },
+            }}
+          >
+            <div className="text-secondary justify-content-center align-content-center">
+              <div className="pb-3">
+                <AiOutlineMail
+                  color="#1A1A1A"
+                  size={25}
+                  className="inline-flex pr-2"
+                />
+                phakin.buddha@gmail.com
+              </div>
+              <div className="pt-3">
+                <BsFillTelephoneFill
+                  color="#1A1A1A"
+                  size={25}
+                  className="inline-flex pr-2"
+                />
+                086-398-5005
+              </div>
             </div>
           </div>
-          <div
-            className="col-md-6 text-secondary align-items-center justify-content-center flex"
-            style={styles}
-          >
-            <button
-              href="https://www.facebook.com/phakin.buddha/"
-              className="text-dark m-1"
-            >
-              <FaFacebookF color="#1A1A1A" size={25} />
-            </button>
-            <button
-              href="https://www.instagram.com/_sshutter/"
-              className="text-dark m-1"
-            >
-              <FaInstagram color="#1A1A1A" size={25} />
-            </button>
-            <button href="#" className="text-dark m-1" onClick={openGmail}>
-              <FaEnvelope color="#1A1A1A" size={25} />
-            </button>
-            <button
-              href="https://www.linkedin.com/in/phakin-buddha-a24b4823b/"
-              className="text-dark m-1"
-            >
-              <FaLinkedin color="#1A1A1A" size={25} />
-            </button>
-            <button
-              href="https://github.com/sshutter"
-              className="text-dark m-1"
-            >
-              <FaGithub color="#1A1A1A" size={25} />
-            </button>
+          <div className="col-custom py-3">
+            <div className="text-secondary justify-content-center align-content-center">
+              <button
+                className="text-dark m-1 px-2"
+                onClick={handleFacebookClick}
+              >
+                <BsFacebook color="#1A1A1A" size={25} />
+              </button>
+
+              <button
+                className="text-dark m-1 px-2"
+                onClick={handleInstagramClick}
+              >
+                <FaInstagram color="#1A1A1A" size={25} />
+              </button>
+
+              <button className="text-dark m-1 px-2" onClick={openGmail}>
+                <FaEnvelope color="#1A1A1A" size={25} />
+              </button>
+
+              <button
+                className="text-dark m-1 px-2"
+                onClick={handleLinkedinClick}
+              >
+                <FaLinkedin color="#1A1A1A" size={25} />
+              </button>
+
+              <button
+                className="text-dark m-1 px-2"
+                onClick={handleGithubClick}
+              >
+                <FaGithub color="#1A1A1A" size={25} />
+              </button>
+            </div>
           </div>
         </div>
       </div>
